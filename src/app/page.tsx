fc23d1381ee9fb71cefbec38f214544c9a16d5fe
Nimira43 <n.rabone72@aol.com>
2026-04-11 18:56:49 +0100

    Updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
+import { Button, Input, Select } from 'antd'
+
+const options = [
+  { label: 'Appointments', value: 'Appointments'},
+  { label: 'Prescriptions', value: 'Prescriptions'},
+  { label: 'Test Results', value: 'Test Results'},
+  { label: 'Medical History', value: 'Medical History'},
+  { label: 'Physiotheraphy', value: 'Physiotheraphy'},
+]
+
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <div className='p-5 flex flex-col gap-5 w-max'>
+      <h1 className='logo-text text-3xl'>
+        Asclepieia
+      </h1>
+      <Button>Login</Button>
+      <Button type='primary'>Register</Button>
+      <Input
+        placeholder='Search'
+      />
+      <Select
+        options={options}
+      />
+    </div>
   )
 }
