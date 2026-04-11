d97502810aeb5726e664a3956f4b35265de60d54
Nimira43 <n.rabone72@aol.com>
2026-04-11 18:34:21 +0100

    Project created

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import { Poppins } from 'next/font/google'
+import './globals.css'
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+})
+
+export const metadata = {
+  title: 'Asclepieia',
+  description: 'Doctors appointment booking application using Next and MongoDB',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+    <body className={poppins.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dark': '#111',
+        'light': '#ffffee',
+        'grey-1': '#333',
+        'grey-2': '#999', 
+        'grey-3': '#ccc',
+        'grey-4': '#eee'
+      },   
+    },
+  },
+  plugins: [],
+}
+
